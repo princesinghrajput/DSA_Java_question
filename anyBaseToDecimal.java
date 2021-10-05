168bb56c50872327ef11ce8e81e78476efced2cb
Prince Kumar <83268492+princesinghrajput@users.noreply.github.com>
2021-10-05 12:55:28 +0530

    Span in array

--- anyBaseToDecimal.java
+++ anyBaseToDecimal.java
@@ -4,6 +4,7 @@ import java.util.*;
   
   public static void main(String[] args) {
       Scanner scn = new Scanner(System.in);
+      
       int n = scn.nextInt();
       int b = scn.nextInt();
       int d = getValueIndecimal(n, b);
